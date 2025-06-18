b5a22b747a7ee4eb8ebf721b672bc24d4c94b780
franco <francoagustinbarilatti@hotmail.com>
2025-06-18 08:49:19 -0300

    feat: se agrego estructura de clase Platform

--- src/main/cineverse/models/Platform.java
+++ src/main/cineverse/models/Platform.java
@@ -1,4 +1,74 @@
 package main.cineverse.models;
 
+import java.util.List;
+
 public class Platform {
+
+    private String name;
+    private List<Movie> movies;
+    private List<User> users;
+    private List<Viewing> viewings;
+
+    public Platform() {
+    }
+
+    public Platform(String name, List<Movie> movies, List<User> users, List<Viewing> viewings) {
+        this.name = name;
+        this.movies = movies;
+        this.users = users;
+        this.viewings = viewings;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<Viewing> getViewings() {
+        return viewings;
+    }
+
+    public void setViewings(List<Viewing> viewings) {
+        this.viewings = viewings;
+    }
+
+    @Override
+    public String toString() {
+
+        String movieTitles = movies.stream()
+                .map(Movie::getTitle)
+                .toList()
+                .toString();
+
+        String usernames = users.stream()
+                .map(User::getUsername)
+                .toList()
+                .toString();
+
+        return "Platform{" +
+                "name='" + name + '\'' +
+                ", movies=" + movieTitles +
+                ", users=" + usernames +
+                '}';
+    }
 }
