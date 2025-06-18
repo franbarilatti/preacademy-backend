0c80d77ea319862bf6611d0af2cf65792a877608
franco <francoagustinbarilatti@hotmail.com>
2025-06-18 08:41:11 -0300

    feat: Se agrego estructura de clases User y relacionadas

--- src/main/cineverse/models/User.java
+++ src/main/cineverse/models/User.java
@@ -1,4 +1,101 @@
 package main.cineverse.models;
 
+import main.cineverse.models.enums.SubcriptionPlan;
+
+import java.util.List;
+
 public class User {
+
+    private int id;
+    private String username;
+    private SubcriptionPlan plan;
+    private String country;
+    private List<Viewing> viewings;
+
+    public User() {
+    }
+
+    public User(int id, String username, SubcriptionPlan plan, String country, List<Viewing> viewings) {
+        this.id = id;
+        this.username = username;
+        this.plan = plan;
+        this.country = country;
+        this.viewings = viewings;
+    }
+
+    public User id(int id){
+        this.id = id;
+        return this;
+    }
+
+    public User username(String username){
+        this.username = username;
+        return this;
+    }
+
+    public User plan(SubcriptionPlan plan){
+        this.plan = plan;
+        return this;
+    }
+
+    public User country(String country){
+        this.country = country;
+        return this;
+    }
+
+    public User viewings(List<Viewing> viewings){
+        this.viewings = viewings;
+        return this;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public SubcriptionPlan getPlan() {
+        return plan;
+    }
+
+    public void setPlan(SubcriptionPlan plan) {
+        this.plan = plan;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public List<Viewing> getViewings() {
+        return viewings;
+    }
+
+    public void setViewings(List<Viewing> viewings) {
+        this.viewings = viewings;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", plan=" + plan +
+                ", country='" + country + '\'' +
+                ", viewings=" + viewings +
+                '}';
+    }
 }
--- src/main/cineverse/models/Viewing.java
+++ src/main/cineverse/models/Viewing.java
@@ -6,7 +6,77 @@ public class Viewing {
     Movie movie;
     User user;
     LocalDate date;
-    int minutesWatched
+    int minutesWatched;
 
+    public Viewing() {
+    }
 
+    public Viewing(Movie movie, User user, LocalDate date, int minutesWatched) {
+        this.movie = movie;
+        this.user = user;
+        this.date = date;
+        this.minutesWatched = minutesWatched;
+    }
+
+    public Viewing movie(Movie movie){
+        this.movie = movie;
+        return this;
+    }
+
+    public Viewing user(User user){
+        this.user = user;
+        return this;
+    }
+
+    public Viewing date(LocalDate date){
+        this.date = date;
+        return this;
+    }
+
+    public Viewing minutesWatched(int minutesWatched){
+        this.minutesWatched = minutesWatched;
+        return this;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(Movie movie) {
+        this.movie = movie;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getMinutesWatched() {
+        return minutesWatched;
+    }
+
+    public void setMinutesWatched(int minutesWatched) {
+        this.minutesWatched = minutesWatched;
+    }
+
+    @Override
+    public String toString() {
+        return "Viewing{" +
+                "movie=" + movie.getTitle() +
+                ", user=" + user.getUsername() +
+                ", date=" + date +
+                ", minutesWatched=" + minutesWatched +
+                '}';
+    }
 }
