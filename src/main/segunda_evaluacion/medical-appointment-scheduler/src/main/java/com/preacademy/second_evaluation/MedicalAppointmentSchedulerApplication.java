91abc6583ef7cc4fc00e2e06bb97addecd90a7fd
franco <francoagustinbarilatti@hotmail.com>
2025-08-05 14:19:44 -0300

    feat: Se crearon clases de test para los services

---
+++ src/main/segunda_evaluacion/medical-appointment-scheduler/src/main/java/com/preacademy/second_evaluation/MedicalAppointmentSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.preacademy.second_evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicalAppointmentSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicalAppointmentSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/main/segunda_evaluacion/medical-appointment-scheduler/src/test/java/com/preacademy/second_evaluation/MedicalAppointmentSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preacademy.second_evaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalAppointmentSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
